d2a3fa2219381948fc59ac0a4c617ef26656f1da
AlpinderSidhu <asingh28693@gmail.com>
2025-05-03 14:23:43 -0500

    Added updateImage API

--- src/main/java/com/ecommerce/sb_ecom/controller/ProductController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/ProductController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 
 @RestController
@@ -49,8 +52,15 @@ public class ProductController {
     }
 
     @DeleteMapping("admin/products/{productId}")
-    public HttpEntity<ProductDTO> deleteProduct( @PathVariable Long productId){
+    public HttpEntity<ProductDTO> deleteProduct(@PathVariable Long productId) {
         ProductDTO deleteProductDTO = productService.deleteProduct(productId);
         return new ResponseEntity<>(deleteProductDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("products/{productId}/image")
+    public ResponseEntity<ProductDTO> updateProductImage(@PathVariable Long productId,
+                                                         @RequestParam("image") MultipartFile image) throws IOException {
+        ProductDTO updatedProduct = productService.updateProductImage(productId, image);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/sb_ecom/service/ProductService.java
+++ src/main/java/com/ecommerce/sb_ecom/service/ProductService.java
@@ -2,12 +2,22 @@ package com.ecommerce.sb_ecom.service;
 
 import com.ecommerce.sb_ecom.payload.ProductDTO;
 import com.ecommerce.sb_ecom.payload.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface ProductService {
     ProductDTO addProduct(Long categoryId, ProductDTO product);
+
     ProductResponse getAllProducts();
+
     ProductResponse searchByCategory(Long categoryId);
+
     ProductResponse searchProductByKeyword(String keyword);
+
     ProductDTO updateProduct(Long productId, ProductDTO product);
+
     ProductDTO deleteProduct(Long productId);
+
+    ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException;
 }
